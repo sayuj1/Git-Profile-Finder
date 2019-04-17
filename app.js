2e867a9ce99e6220c9227417cdec40fa034f6f3d
Sayuj Sehgal <sayujsehgal@iprakash.com>
2019-04-17 07:44:55 +0530

    basic pagination added | showuser created

---
+++ app.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const app = express();
+const request = require('request');
+const parse = require('parse-link-header');
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+//Routes
+
+app.get("/", function(req, res){
+    //display homepage
+    res.render("index");
+});
+
+
+app.get("/show/:user", function(req, res){
+    let user = req.params.user;
+    //console.log(user);
+   var options = {
+       url: "http://api.github.com/search/users?q="+user+"&page=1&per_page=7",
+       headers:{
+           'user-agent':  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
+       },
+       data:{
+           client_id: 'eb37eae804d2c4b69980',
+           client_secret: '869f459c6e3f3d0f8413e34f1443e6512a70d58a'
+       },
+   };
+   function callback(error, response, body){
+       if(!error && response.statusCode == 200){
+          // console.log(body);
+          //console.log(response.headers['link']); //getting value of link header
+          const page = parse(response.headers['link']);
+          console.log(page);
+           const userInfo = JSON.parse(body);
+           res.render("show", {data: userInfo, page: page, page_no: '1'});  //https://api.github.com/search/users?q='+user+'&page=2&per_page=5
+       }
+       else{
+           //console.log(req.headers);
+           //console.log(body);
+           console.log(error);
+       }
+   }
+   request(options, callback);
+   /*request('http://api.github.com/search/users?q=sayuj+user:mozilla', function(error, response, body){
+    console.log(body);
+   }); */
+});
+
+app.get("/show/:user/:id", function(req, res){
+    //console.log(req.params.user);
+    //console.log(req.params.id);
+    let user = req.params.user;
+    let id = req.params.id;
+    //console.log(user);
+   var options = {
+       url: "http://api.github.com/search/users?q="+user+"&page="+id+"&per_page=7",
+       headers:{
+           'user-agent':  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36',
+       },
+       data:{
+           client_id: 'eb37eae804d2c4b69980',
+           client_secret: '869f459c6e3f3d0f8413e34f1443e6512a70d58a'
+       },
+   };
+   function callback(error, response, body){
+       if(!error && response.statusCode == 200){
+          // console.log(body);
+          //console.log(response.headers['link']); //getting value of link header
+          const page = parse(response.headers['link']);   //converting link header value in JSON format
+          console.log(page);
+           const userInfo = JSON.parse(body);
+           res.render("show", {data: userInfo, page: page, page_no: id});  //https://api.github.com/search/users?q='+user+'&page=2&per_page=5
+       }
+       else{
+           //console.log(req.headers);
+           //console.log(body);
+           console.log(error);
+       }
+   }
+   request(options, callback);
+});
+
+const server = app.listen(3000, function(){
+console.log('server started at port 3000');
+});
